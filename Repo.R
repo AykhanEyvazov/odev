a9c0d518fc896045d4a0316f84732b8663ab473c
Aykhan Eyvazov <aykhan.eyvaz.04@gmail..com>
2023-03-22 13:00:51 +0300

    ommit2

---
+++ Repo.R
@@ -0,0 +1,46 @@
+---
+  title: "Ödev 1"
+author: "Ad Soyad"
+---
+  
+  ```{r global_options, include=FALSE}
+# Bu kod blogununun icerigini degistirmeyiniz, silmeyiniz.
+library(knitr)
+library(ggplot2)
+opts_chunk$set(fig.align="center", fig.height=4, fig.width=5.5)
+```
+
+**Soru 1** Markdown kullanarak bir kelimesi kalın olan bir cümle yazınız.
+
+(**Mulkiye** tarihi keçmişi olan bir okukldur)! 
+  
+  **Soru 2** Markdown kullanarak bir kelimesi italik olan bir cümle yazınız.
+
+(*Mulkiye* tarihi keçmişi olan bir okukldur)! 
+  
+  **Soru 3** Markdown kullanarak bir kelimesi kalın italik olan bir cümle yazınız.
+
+(**_Mulkiye_** tarihi keçmişi olan bir okukldur)!
+  
+  **Soru 4** Markdown kullanarak 3 elemanlı bir sırasız liste yazınız.
+
+Cevabınızı buraya yazınız!
+  
+  **Soru 5** Markdown kullanarak 3 elemanlı bir sıralı liste yazınız.
+
+Cevabınızı buraya yazınız!
+  
+  **Soru 6** Markdown kullanarak istediğiniz bir websayfasına link veriniz.
+
+([Mülkiye](http://www.politics.ankara.edu.tr/)!
+    
+    **Soru 7** Markdown kullanarak bir komik bir kedi fotoğrafı ekleyiniz.
+  
+  !{Sirin kedi} !(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sabah.com.tr%2Fyasam%2Finternet-fenomeni-ofkeli-kedi-koyuki-3454876&psig=AOvVaw2he9PDJtNgTSj6mNlbCrF7&ust=1679563067622000&source=images&cd=vfe&ved=0CBEQjhxqFwoTCLC4zZOa7_0CFQAAAAAdAAAAABAE)!
+    
+    **Soru 8** RMarkdown kullanarak bir kod bloğu ekleyiniz ve içerisinde basit bir matematik işlemi yapınız.
+  
+  !
+    ```{r}
+  4+0
+  ```
